01db920b618270fbef487f9b0e7982a176463394
Ben Solwitz <notalamer@gmail.com>
2020-07-11 14:05:44 -0400

    modified setup scripts to use workspace directory

--- host-setup.sh
+++ host-setup.sh
@@ -3,6 +3,14 @@
 PATH='/opt/conda/bin:/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
 if [ ! -d "apex" ]; then
+    # get celeba data
+    DEBIAN_FRONTEND=noninteractive apt-get -yq update
+    DEBIAN_FRONTEND=noninteractive apt-get -yq install wget unzip git
+
+    echo "getting data"
+    wget --quiet --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1g9tAydWEN0PW-BYxdhNuGG9vXrdNzffA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1g9tAydWEN0PW-BYxdhNuGG9vXrdNzffA" -O img_align_celeba.zip && rm -rf /tmp/cookies.txt
+    unzip -q img_align_celeba.zip -d /workspace/imgs
+
     # install apex
     git clone https://github.com/NVIDIA/apex.git
     pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./apex
@@ -12,12 +20,4 @@ if [ ! -d "apex" ]; then
 
     # get some more python libraries
     pip install matplotlib scipy
-
-    # get celeba data
-    DEBIAN_FRONTEND=noninteractive apt-get -yq update
-    DEBIAN_FRONTEND=noninteractive apt-get -yq install wget unzip
-
-    echo "getting data"
-    wget --quiet --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1g9tAydWEN0PW-BYxdhNuGG9vXrdNzffA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1g9tAydWEN0PW-BYxdhNuGG9vXrdNzffA" -O img_align_celeba.zip && rm -rf /tmp/cookies.txt
-    unzip -q img_align_celeba.zip -d /root/imgs
 fi
--- vast-setup.sh
+++ vast-setup.sh
@@ -6,6 +6,6 @@
 # ./vast-setup 5 10444
 # for ssh5.vast.ai:10444
 
-scp -P $2 host-setup.sh *.py root@ssh$1.vast.ai:
+scp -P $2 host-setup.sh *.py root@ssh$1.vast.ai:/workspace
 ssh -p $2 root@ssh$1.vast.ai -L 8080:localhost:8080 "chmod 774 host-setup.sh; ./host-setup.sh"
 ssh -p $2 root@ssh$1.vast.ai -L 8080:localhost:8080
