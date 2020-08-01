43055f90995b6f579de38ba2fd871d2992cf9e72
Ben Solwitz <notalamer@gmail.com>
2020-08-01 11:52:03 -0400

    moved working directory to root home instead of workspace added env variables for vast host num and port

--- host-setup.sh
+++ host-setup.sh
@@ -2,14 +2,14 @@
 
 PATH='/opt/conda/bin:/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
-if [ ! -d "/workspace/imgs" ]; then
+if [ ! -d "imgs" ]; then
     # get celeba data
     DEBIAN_FRONTEND=noninteractive apt-get -yq update
     DEBIAN_FRONTEND=noninteractive apt-get -yq install wget unzip git
 
     echo "getting data"
     wget --quiet --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1g9tAydWEN0PW-BYxdhNuGG9vXrdNzffA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1g9tAydWEN0PW-BYxdhNuGG9vXrdNzffA" -O img_align_celeba.zip && rm -rf /tmp/cookies.txt
-    unzip -q img_align_celeba.zip -d /workspace/imgs
+    unzip -q img_align_celeba.zip -d imgs
 
     # install apex
     git clone https://github.com/NVIDIA/apex.git
@@ -22,5 +22,5 @@ if [ ! -d "/workspace/imgs" ]; then
     pip install matplotlib scipy
 fi
 
-pip install /workspace/stylegan2-pytorch
-chmod u+x /workspace/stylegan2-pytorch/bin/stylegan2_pytorch
+pip install stylegan2-pytorch/
+chmod u+x stylegan2-pytorch/bin/stylegan2_pytorch
--- vast-setup.sh
+++ vast-setup.sh
@@ -6,7 +6,7 @@
 # ./vast-setup 5 10444
 # for ssh5.vast.ai:10444
 
-ssh -p $2 root@ssh$1.vast.ai -L 8080:localhost:8080 "DEBIAN_FRONTEND=noninteractive apt-get -yq install rsync"
-rsync -a -e "ssh -p $2" host-setup.sh stylegan2-pytorch root@ssh$1.vast.ai:/workspace
-ssh -p $2 root@ssh$1.vast.ai -L 8080:localhost:8080 "cd /workspace; chmod 774 host-setup.sh; ./host-setup.sh"
-ssh -p $2 root@ssh$1.vast.ai -L 8080:localhost:8080
+ssh -p $VAST_PORT root@ssh$VAST_HOST_NUM.vast.ai -L 8080:localhost:8080 "DEBIAN_FRONTEND=noninteractive apt-get -yq install rsync"
+rsync -a -e "ssh -p $VAST_PORT" host-setup.sh stylegan2-pytorch root@ssh$VAST_HOST_NUM.vast.ai:
+ssh -p $VAST_PORT root@ssh$VAST_HOST_NUM.vast.ai -L 8080:localhost:8080 "chmod 774 host-setup.sh; ./host-setup.sh"
+ssh -p $VAST_PORT root@ssh$VAST_HOST_NUM.vast.ai -L 8080:localhost:8080
